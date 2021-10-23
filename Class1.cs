3437e5b5f5472ee6bd2c64b2c6d85f9666d266ec
unknown <kamil.grosiak@gmail.com>
2021-10-23 12:16:14 +0200

    second commit

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Student
+{
+	public Strudent()
+	{
+	private int id;
+	private String name;
+	private String surname;
+    Strudent(int id,string name,string surname)
+    {
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+    }
+	public void getName(int id)
+    {
+		
+    }
+	}
+}
