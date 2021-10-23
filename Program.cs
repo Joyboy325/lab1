0e97df324849247a71525690e8119f133f1fb1e4
unknown <kamil.grosiak@gmail.com>
2021-10-23 12:39:37 +0200

    nowe

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Student x = new Student(1, "xxx", "yyy");
+            x.getName();
         }
     }
 }
--- Student.cs
+++ Student.cs
@@ -2,20 +2,17 @@
 
 public class Student
 {
-	public Strudent()
-	{
 	private int id;
 	private String name;
 	private String surname;
-    Strudent(int id,string name,string surname)
+    public Student(int id,string name,string surname)
     {
 		this.id = id;
 		this.name = name;
 		this.surname = surname;
     }
-	public void getName(int id)
+	public void getName()
     {
-		
+		Console.WriteLine(this.name);
     }
-	}
 }
